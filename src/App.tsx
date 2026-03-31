d107372d18b96d6f9ac67c9c0eb34ab3531c8e69
JAMES-RY <jamesrymerbagasbasocero@gmail.com>
2026-03-31 13:28:56 +0800

    added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+
 import Home from './pages/Home';
+import Login from './pages/login';
+import Menu from './pages/Menu';
+import Page1 from './pages/Page1';
+import Page2 from './pages/Page2';
+import Template from './pages/Template';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
-/* Basic CSS for apps built with Ionic */
+/* Basic CSS */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
 
-/* Optional CSS utils that can be commented out */
+/* Optional CSS */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
@@ -19,18 +25,10 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
+/* Dark mode */
 import '@ionic/react/css/palettes/dark.system.css';
 
-/* Theme variables */
+/* Theme */
 import './theme/variables.css';
 
 setupIonicReact();
@@ -39,12 +37,41 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
+
+        {/* Login first */}
+        <Route exact path="/login">
+          <Login />
+        </Route>
+
+        {/* Home */}
         <Route exact path="/home">
           <Home />
         </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
+
+        {/* Menu */}
+        <Route exact path="/menu">
+          <Menu />
         </Route>
+
+        {/* Pages */}
+        <Route exact path="/page1">
+          <Page1 />
+        </Route>
+
+        <Route exact path="/page2">
+          <Page2 />
+        </Route>
+
+        {/* Template */}
+        <Route exact path="/template">
+          <Template />
+        </Route>
+
+        {/* Default route */}
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,15 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
-
+import React from 'react';
+import { IonButton } from '@ionic/react';
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+     <>
+      <IonButton expand="block">Block</IonButton>
+      <IonButton expand="full">Full</IonButton>
+    </>
   );
 };
+
 
 export default Home;
