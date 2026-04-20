4b944b215548c049f2f58cafaa8d3b8f562870c3
JAMES-RY <jamesrymerbagasbasocero@gmail.com>
2026-04-20 14:42:41 +0800

    home tab

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,17 +1,26 @@
-import { IonHeader, IonPage, IonTitle,IonToolbar } from "@ionic/react";
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 
-const Page1: React.FC =() =>{
 
-    return(
-      <IonPage>
-        <IonHeader>
-         <IonToolbar>
-            <IonTitle> Page 1</IonTitle>
-         </IonToolbar>
+const Page1: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
         </IonHeader>
-      </IonPage>
-
-    );
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Page1;
